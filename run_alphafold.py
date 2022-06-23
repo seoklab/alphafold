seoklab/alphafold

commit a33d0384aa621cab8a35f2ac291fcc5e9c7bf245

Add option for multistate GPCR modeling

run_alphafold.py
@@ -83,11 +83,21 @@ uniclust30_database_path = os.path.join(
 uniprot_database_path = os.path.join(DOWNLOAD_DIR, 'uniprot', 'uniprot.fasta')
 
 # Path to the PDB70 database for use by HHsearch.
-pdb70_database_path = os.path.join(DOWNLOAD_DIR, 'pdb70', 'pdb70')
+pdb70_database_paths = {
+  "normal": os.path.join(DOWNLOAD_DIR, 'pdb70', 'pdb70'),
+  "active": os.path.join(DOWNLOAD_DIR, 'gpcr100', 'GPCR100.Active'),
+  "intermediate": os.path.join(DOWNLOAD_DIR, 'gpcr100', 'GPCR100.Intermediate'),
+  "inactive": os.path.join(DOWNLOAD_DIR, 'gpcr100', 'GPCR100.Inactive'),
+}
 
 # Path to the PDB seqres database for use by hmmsearch.
-pdb_seqres_database_path = os.path.join(
-  DOWNLOAD_DIR, 'pdb_seqres', 'pdb_seqres.txt')
+_pdb_seqres_dir = os.path.join(DOWNLOAD_DIR, 'pdb_seqres')
+pdb_seqres_database_paths = {
+  "normal": os.path.join(_pdb_seqres_dir, 'pdb_seqres.txt'),
+  "active": os.path.join(_pdb_seqres_dir, 'pdb_seqres_active.txt'),
+  "intermediate": os.path.join(_pdb_seqres_dir, 'pdb_seqres_intermediate.txt'),
+  "inactive": os.path.join(_pdb_seqres_dir, 'pdb_seqres_inactive.txt'),
+}
 
 # Path to a directory with template mmCIF structures, each named <pdb_id>.cif')
 template_mmcif_dir = os.path.join(DOWNLOAD_DIR, 'pdb_mmcif', 'mmcif_files')
@@ -116,6 +126,8 @@ flags.DEFINE_integer('model_cnt', 5, 'Counts of models to use. Note that '
 flags.DEFINE_integer('nproc', nproc, 'Maximum cpu count to use. Note that '
                      'the actual cpu load might be different than '
                      'the configured value.')
+flags.DEFINE_boolean('jit', False,
+                     'Whether to jit compile the alphafold model.')
 flags.DEFINE_string('max_template_date', date.today().isoformat(),
                     'Maximum template release date to consider'
                     '(ISO-8601 format - i.e. YYYY-MM-DD). '
@@ -133,6 +145,11 @@ flags.DEFINE_enum('model_type', 'normal',
                   '8 model ensemblings (casp14).\n'
                   'Note that the casp14 preset is just an alias of '
                   '--model_type=normal --ensemble=8 option.')
+flags.DEFINE_enum('state', 'normal',
+                  tuple(pdb70_database_paths),
+                  'Choose state for GPCRs. Will be ignored if '
+                  '--pdb70_database_path (monomer) or '
+                  '--pdb_seqres_database_path (multimer) argument is set.')
 flags.DEFINE_integer('num_multimer_predictions_per_model', 5, 'How many '
                      'predictions (each with a different random seed) will be '
                      'generated per model. E.g. if this is 2 and there are 5 '
@@ -182,9 +199,9 @@ flags.DEFINE_string('uniclust30_database_path', uniclust30_database_path,
                     'Path to the Uniclust30 database for use by HHblits.')
 flags.DEFINE_string('uniprot_database_path', uniprot_database_path,
                     'Path to the Uniprot database for use by JackHMMer.')
-flags.DEFINE_string('pdb70_database_path', pdb70_database_path,
+flags.DEFINE_string('pdb70_database_path', None,
                     'Path to the PDB70 database for use by HHsearch.')
-flags.DEFINE_string('pdb_seqres_database_path', pdb_seqres_database_path,
+flags.DEFINE_string('pdb_seqres_database_path', None,
                     'Path to the PDB seqres database for use by hmmsearch.')
 flags.DEFINE_string('template_mmcif_dir', template_mmcif_dir,
                     'Path to a directory with template mmCIF structures, '
@@ -201,19 +218,17 @@ FLAGS = flags.FLAGS
 # yapf: enable
 
 
-def check_nvidia_cache():
-  pass
-
-
-if devices.BACKEND != "cpu":
+if devices.BACKEND == "cpu":
+  def check_nvidia_cache():
+    pass
+else:
   _NFS_CACHE = frozenset(
       os.stat(pi.mountpoint).st_dev
       for pi in psutil.disk_partitions(all=True)
       if pi.fstype == 'nfs')
 
   if _NFS_CACHE:
-
-    def check_nvidia_cache():  # noqa: F811
+    def check_nvidia_cache():
       nvidia_cachedir = os.path.expanduser('~/.nv')
       nvidia_cachedir_non_nfs = os.path.expandvars("/tmp/$USER/nv")
 
@@ -282,7 +297,8 @@ def predict_structure_permodel(
   model_params = data.get_model_haiku_params(model_id=model_id,
                                              model_type=model_type,
                                              data_dir=data_dir)
-  model_runner = model.RunModel(model_config, model_params, device=device)
+  model_runner = model.RunModel(
+      model_config, model_params, device=device, jit_compile=FLAGS.jit)
   with profiler(f'process_features_{model_name}'):
     processed_feature_dict = model_runner.process_features(
         feature_dict, random_seed)
@@ -539,6 +555,9 @@ def main(fasta_paths: List[str]):
   if len(fasta_names) != len(set(fasta_names)):
     raise ValueError('All FASTA paths must have a unique basename.')
 
+  if FLAGS.state != "normal":
+    fasta_names = [f"{name}_{FLAGS.state}" for name in fasta_names]
+
   logging.set_verbosity(logging.INFO)
   if FLAGS.debug:
     logging.set_verbosity(logging.DEBUG)
@@ -559,10 +578,13 @@ def main(fasta_paths: List[str]):
       FLAGS.ensemble = 8
 
   if run_multimer_system:
+    if (pdb_seqres_database_path := FLAGS.pdb_seqres_database_path) is None:
+      pdb_seqres_database_path = pdb_seqres_database_paths[FLAGS.state]
+
     template_searcher = hmmsearch.Hmmsearch(
         binary_path=FLAGS.hmmsearch_binary_path,
         hmmbuild_binary_path=FLAGS.hmmbuild_binary_path,
-        database_path=FLAGS.pdb_seqres_database_path,
+        database_path=pdb_seqres_database_path,
         n_cpu=FLAGS.nproc)
     template_featurizer = templates.HmmsearchHitFeaturizer(
         mmcif_dir=FLAGS.template_mmcif_dir,
@@ -572,9 +594,12 @@ def main(fasta_paths: List[str]):
         release_dates_path=None,
         obsolete_pdbs_path=FLAGS.obsolete_pdbs_path)
   else:
+    if (pdb70_database_path := FLAGS.pdb70_database_path) is None:
+      pdb70_database_path = pdb70_database_paths[FLAGS.state]
+
     template_searcher = hhsearch.HHSearch(
         binary_path=FLAGS.hhsearch_binary_path,
-        databases=[FLAGS.pdb70_database_path],
+        databases=[pdb70_database_path],
         n_cpu=FLAGS.nproc)
     template_featurizer = templates.HhsearchHitFeaturizer(
         mmcif_dir=FLAGS.template_mmcif_dir,

scripts/download_all_data.sh
@@ -51,6 +51,9 @@ bash "${SCRIPT_DIR}/download_mgnify.sh" "${DOWNLOAD_DIR}"
 echo "Downloading PDB70..."
 bash "${SCRIPT_DIR}/download_pdb70.sh" "${DOWNLOAD_DIR}"
 
+echo "Downloading GPCR100..."
+bash "${SCRIPT_DIR}/download_gpcr100.sh" "${DOWNLOAD_DIR}"
+
 echo "Downloading PDB mmCIF files..."
 bash "${SCRIPT_DIR}/download_pdb_mmcif.sh" "${DOWNLOAD_DIR}"
 

scripts/download_gpcr100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2021 DeepMind Technologies Limited
+# Copyright (C) 2022 Lab of Computational Biology and Biomolecular Engineering
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Downloads and unzips the PDB SeqRes database for AlphaFold.
+# Downloads and unzips the GPCR100 database for AlphaFold.
 #
-# Usage: bash download_pdb_seqres.sh /path/to/download/directory
+# Usage: bash download_gpcr100.sh /path/to/download/directory
 set -e
 
 if [[ $# -eq 0 ]]; then
@@ -29,12 +29,15 @@ if ! command -v aria2c &> /dev/null ; then
     exit 1
 fi
 
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
 DOWNLOAD_DIR="$1"
-ROOT_DIR="${DOWNLOAD_DIR}/pdb_seqres"
-SOURCE_URL="ftp://ftp.pdbj.org/pub/pdb/derived_data/pdb_seqres.txt"
+ROOT_DIR="${DOWNLOAD_DIR}/gpcr100"
+SOURCE_URL_PREFIX="https://zenodo.org/record/5745217/files/"
 
-mkdir --parents "${ROOT_DIR}"
-aria2c -x16 -j16 --allow-overwrite "${SOURCE_URL}" --dir="${ROOT_DIR}"
-"$SCRIPT_DIR/filter_pdb_seqres.py" "${ROOT_DIR}/pdb_seqres.txt"
+mkdir -p "${ROOT_DIR}"
+for state in Active Inactive Intermediate; do
+	aria2c "${SOURCE_URL_PREFIX}/GPCR100.${state}.tgz" --dir="${ROOT_DIR}"
+
+	archive="${ROOT_DIR}/GPCR100.${state}.tgz"
+	tar -xzf "${archive}" -C "${ROOT_DIR}"
+	rm -f "${archive}"
+done

scripts/filter_pdb_seqres.py
@@ -1,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+gpcrdb_path = Path(__file__).resolve().parent / "GPCRdb.csv"
+
+
+def chain_list_from_df(df: pd.DataFrame, state: str):
+    """
+    Converts a dataframe with a column of chain names to a list of chain names.
+    """
+    state_pdbs = df[df["State"] == state]
+    pdb_chains = state_pdbs[["PDB", "Preferred"]]
+    return [f"{pdb.lower()}_{chain}"
+            for pdb, chain in pdb_chains.itertuples(index=False, name=None)]
+
+
+def filter_seqres(seqres_path: str, chains: List[str]) -> List[str]:
+  chains_set = set(chains)
+  ret = []
+  with open(seqres_path) as f:
+    for line in f:
+      if line[1:7] in chains_set:
+        ret += [line, next(f)]
+  return ret
+
+
+def main():
+  seqres_path = sys.argv[1]
+  seqres_dir = Path(seqres_path).resolve(True).parent
+
+  gpcrdb = pd.read_csv(gpcrdb_path)
+  for state in ["Active", "Inactive", "Intermediate"]:
+    chains = chain_list_from_df(gpcrdb, state)
+    filtered_seqres = filter_seqres(seqres_path, chains)
+    with open(seqres_dir / f"pdb_seqres_{state.lower()}.txt", "w") as f:
+      f.write("".join(filtered_seqres))
+
+
+if __name__ == "__main__":
+  main()

commit e608ae675eb3957ef0fb099adfbf195cea426c7d

Fix bug in run_alphafold.py
This is a regression introduced in a33d0384aa621cab8a35f2ac291fcc5e9c7bf245.

run_alphafold.py
@@ -218,17 +218,18 @@ FLAGS = flags.FLAGS
 # yapf: enable
 
 
-if devices.BACKEND == "cpu":
-  def check_nvidia_cache():
-    pass
-else:
+def check_nvidia_cache():
+  pass
+
+
+if devices.BACKEND != "cpu":
   _NFS_CACHE = frozenset(
       os.stat(pi.mountpoint).st_dev
       for pi in psutil.disk_partitions(all=True)
       if pi.fstype == 'nfs')
 
   if _NFS_CACHE:
-    def check_nvidia_cache():
+    def check_nvidia_cache():  # noqa: F811
       nvidia_cachedir = os.path.expanduser('~/.nv')
       nvidia_cachedir_non_nfs = os.path.expandvars("/tmp/$USER/nv")
 