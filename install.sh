12df88e9cb3b9519d0750c6e76217f670c859964
jnooree <jnooree@snu.ac.kr>
2023-03-17 13:53:28 +0900

    chore: bump jax & jaxlib version to 0.3.25

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ conda activate alphafold2 || exit 1
 
 set -eu
 
-pip install --upgrade 'jax>=0.3.17,<0.4' 'jaxlib==0.3.15+cuda11.cudnn82' \
+pip install --upgrade 'jax>=0.3.25,<0.4' 'jaxlib==0.3.25+cuda11.cudnn82' \
             -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 pushd "$CONDA_PREFIX/lib/python3.8/site-packages"
